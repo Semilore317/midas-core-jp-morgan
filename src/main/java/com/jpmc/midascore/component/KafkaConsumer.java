668c47ab6ce6d5f5f550ba063e037816016128da
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-05-07 12:18:46 +0100

    feat: implemented the API call in the kafka consumer

--- src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -2,12 +2,14 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 
 @Component
@@ -15,10 +17,12 @@ public class KafkaConsumer {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
     private final DatabaseConduit databaseConduit;
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
-    public KafkaConsumer(DatabaseConduit databaseConduit, UserRepository userRepository) {
+    public KafkaConsumer(DatabaseConduit databaseConduit, UserRepository userRepository, RestTemplate restTemplate) {
         this.databaseConduit = databaseConduit;
         this.userRepository = userRepository;
+        this.restTemplate = restTemplate;
     }
 
     @KafkaListener(topics = "${general.kafka-topic}")
@@ -29,14 +33,28 @@ public class KafkaConsumer {
         TransactionRecord transactionRecord = new TransactionRecord(sender, recipient, transaction.getAmount());
 
         if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
+            Incentive incentive = restTemplate.postForObject("http://localhost:8080/incentive", transaction, Incentive.class);
+
+            float incentiveAmount;
+
+            if(incentive != null){
+                incentiveAmount = incentive.getAmount();
+            }else{
+                incentiveAmount = 0;
+            }
+            
             sender.setBalance(sender.getBalance() - transaction.getAmount());
             recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
             transactionRecord.setAmount(transaction.getAmount());
             transactionRecord.setRecipient(recipient);
             transactionRecord.setSender(sender);
+            transactionRecord.setIncentive(incentiveAmount);
+
             databaseConduit.save(sender);
             databaseConduit.save(recipient);
             databaseConduit.save(transactionRecord);
+
         }
     }
 }
